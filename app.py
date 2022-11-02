a190420c6a7bdecdca21c3f19f46d4e22542df5d
nikhil namdev ghase <97328668+Nickghase@users.noreply.github.com>
2022-11-02 15:23:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ from sklearn.feature_extraction.text import TfidfTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 from sklearn.pipeline import Pipeline
 import spacy
-import spacy_stramlit
+import spacy_streamlit
 from collections import Counter
 import pickle
 import streamlit as st
